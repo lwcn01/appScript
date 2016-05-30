0ee245bb7bd73b012f182f44bef45e60f4a4fda3
new <lwcn01@163.com>
2016-05-30 17:39:09 +0800

    Delete mem.bat

--- Script/mem.bat
+++
@@ -1,6 +0,0 @@
-set process=cn.microinvestment.weitou
-@adb sehll dumpsys meminfo %process% | findstr "Pss"
-:m
-@adb sehll dumpsys meminfo %process% | findstr "TOTAL"
-@ping -n 5 127.1>nul
-@goto m
